8fd4836adc633ea113396bcfb85ba79e91ca3ca6
dimkonko <d.karacheban@gmail.com>
2013-09-25 18:21:05 +0300

    Changed some issues

---
+++ LearningToday/learningtoday/static/js/main.js
@@ -0,0 +1,42 @@
+
+function getRandom() {
+	var temp = Math.floor((Math.random() * 255));
+	return temp;
+}
+
+function changeToRandom(name) {
+	var temp = "rgb(" + getRandom() + "," + getRandom() + "," + getRandom() + ")";
+	var temp_rgba = ("rgba" + temp.slice(3,-1) + ",0.65);");
+	document.getElementById(name).style.backgroundColor = temp;
+	
+/* 	alert(temp_rgba); */
+	var css = '#' + name + ':hover{box-shadow:0 0 0px 5px ' + temp_rgba + '}';
+	style = document.createElement('style');
+	if (style.styleSheet) style.styleSheet.cssText = css;
+	else style.appendChild(document.createTextNode(css));
+	document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+function hide_text() {
+	document.getElementById("main_search_form").placeholder = "";
+}
+
+function show_text() {
+	document.getElementById("main_search_form").placeholder = "Search courses";
+}
+
+function changeBoxAll() {
+		var elements = document.getElementsByClassName("main_box_style");
+		for (var i = 0, len = elements.length; i < len; i++) {
+			changeToRandom(elements[i].id); /* 		alert(elements[i].id) */
+		}
+	}
+
+function changeBoxOne(name) {
+	changeToRandom(name.id);
+}
+
+window.onload = function()
+{
+	changeBoxAll()	
+}
