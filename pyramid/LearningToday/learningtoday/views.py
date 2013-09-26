db2f5470d9763bf655a0bb22a4e65cec88e283d5
Andriy <voronin.py@gmail.com>
2013-09-26 12:48:50 +0300

    Learning Today project, initial commit
    1) authorization and authentication working
    2) js checks working - checks if all fields are filled
    3) js hashes password
    4) pyramid receives POST Request and checks if there is such user with
    matching password in MySQL db
    5) redirects on success and on failure
    6) if login succeded pyramid sends cookie
    7) logout works, it cleans cookies and redirects to login page
    8) signup works on the side of pyramid, it adds users to db, and
    redirects to home page, doesnt have second field in form for check if
    passwords match
    9) templates aren't split into three parts yet, will be finished soon
    ------------
    js file login.js written by Dima Karacheban
    js file main.js written by Andriy Voronin
    python/pyramid code written by Andriy Voronin

---
+++ pyramid/LearningToday/learningtoday/views.py
@@ -0,0 +1,142 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.security import (
+    authenticated_userid,
+    remember,
+    forget
+    )
+
+from .models import (
+    DBSession,
+    Users,
+    check_login
+    )
+
+menu_links = [{'link': '#', 'text': 'About Us'},
+              {'link': '#', 'text': 'Courses'},
+              {'link': '#', 'text': 'Sign In'},
+              {'link': '/signup', 'text': 'Sign Up'}]
+footer_links = ('About Us', 'Courses', 'Terms')
+
+
+@view_config(renderer='templates/login.pt',
+             request_method="GET",
+             context='pyramid.httpexceptions.HTTPForbidden')
+def login_view(request):
+    return {'app_url': request.application_url,
+            'message': 'Please Login!',
+            'menu_links': menu_links,
+            'footer_links': footer_links}
+
+
+@view_config(renderer='templates/login.pt',
+             request_method="POST",
+             context='pyramid.httpexceptions.HTTPForbidden')
+def login_post(request):
+    login = request.params.get("login")
+    password = request.params.get("hash")
+
+    if check_login(login, password):
+        headers = remember(request, login)
+        return HTTPFound(location='/',
+                         headers=headers)
+    message = 'Failed login'
+    return {'app_url': request.application_url,
+            'message': message,
+            'menu_links': menu_links,
+            'footer_links': footer_links}
+
+
+@view_config(route_name='logout')
+def logout(request):
+    headers = forget(request)
+    return HTTPFound(location='/',
+                     headers=headers)
+
+
+@view_config(route_name='home',
+             renderer='templates/home.pt',
+             permission='view')
+def home(request):
+    userid = authenticated_userid(request)
+    num_boxes = 6
+    if 'num_boxes' in request.params:
+        if request.params['num_boxes']:
+            num_boxes = request.params['num_boxes']
+    return {'app_url': request.application_url,
+            'message': '{}'.format(userid),
+            'menu_links': menu_links[0:2],
+            'footer_links': footer_links,
+            'boxes': generate_box(num_boxes)}
+
+
+@view_config(route_name='signup',
+             renderer='templates/signup.pt',
+             request_method='POST')
+def signup(request):
+    session = DBSession()
+
+    login = request.params.get('login')
+
+    # print '####################################'
+    # for each in request.params:
+    #     print each
+    # print '####################################'
+
+    login_db = session.query(Users).filter_by(login=login).first()
+
+    # password = request.params.get('password')
+    hashed_password = request.params.get('hash')
+    # print '####################################'
+    # print hashed_password
+    # print '####################################'
+
+    if login_db:
+        return {'app_url': request.application_url,
+                'message': 'User with login: {}, already exists.'.format(login),
+                'login': login,
+                'menu_links': menu_links,
+                'footer_links': footer_links}
+        # if password != confirm:
+    #     return {'app_url': request.application_url,
+    #             'message': 'Passwords doesnt match',
+    #             'login': login}
+
+    user = Users(login, hashed_password, 1)
+    session.add(user)
+    headers = remember(request, login)
+    return HTTPFound(location='/', headers=headers)
+
+
+@view_config(route_name='signup',
+             renderer='templates/signup.pt',
+             request_method='GET')
+def signup_form(request):
+    return {'app_url': request.application_url,
+            'message': 'Please SignUp and join the Party!',
+            'login': '',
+            'menu_links': menu_links,
+            'footer_links': footer_links}
+
+
+def generate_box(count):
+    """function that generates boxes,
+    count must be int of how many
+    instances you need
+    return list with dictionaries
+    """
+    count = int(count)
+    if count <= 1:
+        count = 3
+    elif count % 3 == 1:
+        count -= 1
+    elif count % 3 == 2:
+        count += 1
+
+    result = []
+    for i in range(count):
+        div_id = 'box-{}'.format(i)
+        link = 'Link {}'.format(i + 1)
+        result.append({'div_id': div_id, 'link': link})
+    return result
