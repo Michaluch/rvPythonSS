aa640a6a0652e213eeeafd18be66bd4b0862a6d6
adminnew <adminnew@debian.localdomain>
2013-09-26 05:43:47 -0500

    added "LearningToday" html, css, js

---
+++ LearningToday/func1.js
@@ -0,0 +1,49 @@
+function randomNumber(){
+    return Math.round(Math.random() * 255);
+}
+
+function randomColor() {
+    color = "#"+(randomNumber()).toString(16)+(randomNumber()).toString(16)+(randomNumber()).toString(16);
+    return color;
+        
+}
+
+function changeColor() 
+{
+    var a = document.getElementsByClassName('block');
+    
+    for(var i = 0; i < a.length; i++)
+    {
+        a[i].style.background = randomColor();
+    }
+}
+
+window.onload = OnloadFunction
+    
+function OnloadFunction()    
+    {
+    changeColor();
+    
+    var searchform = document.getElementById("search")
+    searchform.onblur = function(){
+        if (this.value == '') {this.setAttribute('value', 'Search courses')};
+        }
+    searchform.onfocus = function() {
+        if (this.value == 'Search courses') {this.setAttribute('value', '')};
+    }
+    
+    var a = document.getElementsByClassName('block');
+    
+    for(var i = 0; i < a.length; i++)
+    {
+        a[i].onmouseover = function() {
+            this.style.background = randomColor();
+            }
+    }
+    
+    b = document.getElementById("buttonchange")
+    b.onclick = function(){
+        changeColor();}
+    }
+
+
