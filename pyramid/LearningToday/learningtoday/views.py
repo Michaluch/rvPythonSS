4bdb283d3fbbef7097da58abf74529f3047e17fe
Andriy <voronin.py@gmail.com>
2013-09-27 23:45:04 +0300

    views rewritten to use classes

--- pyramid/LearningToday/learningtoday/views.py
+++ pyramid/LearningToday/learningtoday/views.py
@@ -1,26 +1,27 @@
-from pyramid.response import Response
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid.security import (
     authenticated_userid,
     remember,
     forget
-    )
+)
 
 from .models import (
     DBSession,
     Users,
     check_login
-    )
+)
 
-menu_links_in = [{'link': '#', 'text': 'About Us'},
-                 {'link': '#', 'text': 'Courses'},
-                 {'link': '/logout', 'text': 'Logout'},
+menu_links_in = [
+    {'link': '#', 'text': 'About Us'},
+    {'link': '#', 'text': 'Courses'},
+    {'link': '/logout', 'text': 'Logout'}
 ]
-menu_links_out = [{'link': '#', 'text': 'About Us'},
-                  {'link': '#', 'text': 'Courses'},
-                  {'link': '#', 'text': 'Sign In'},
-                  {'link': '/signup', 'text': 'Sign Up'},
+menu_links_out = [
+    {'link': '#', 'text': 'About Us'},
+    {'link': '#', 'text': 'Courses'},
+    {'link': '#', 'text': 'Sign In'},
+    {'link': '/signup', 'text': 'Sign Up'}
 ]
 
 footer_links = ('About Us', 'Courses', 'Terms')
@@ -31,113 +32,106 @@ def index(request):
     return {'hello': 'world'}
 
 
-@view_config(renderer='templates/login.pt',
-             request_method="GET",
-             context='pyramid.httpexceptions.HTTPForbidden')
-def login_view(request):
-    return {'app_url': request.application_url,
+class LoginSignUpLogoutViews(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(renderer='templates/login.pt',
+                 request_method="GET",
+                 context='pyramid.httpexceptions.HTTPForbidden')
+    def login_view(self):
+        return {
+            'app_url': self.request.application_url,
             'message': 'Please Login!',
             'menu_links': menu_links_out,
-            'footer_links': footer_links}
+            'footer_links': footer_links
+        }
 
+    @view_config(renderer='templates/login.pt',
+                 request_method="POST",
+                 context='pyramid.httpexceptions.HTTPForbidden')
+    def login_post(self):
+        login = self.request.params.get("login")
+        password = self.request.params.get("hash")
 
-@view_config(renderer='templates/login.pt',
-             request_method="POST",
-             context='pyramid.httpexceptions.HTTPForbidden')
-def login_post(request):
-    login = request.params.get("login")
-    password = request.params.get("hash")
+        if len(password) != 0 and check_login(login, password):
+            headers = remember(self.request, login)
+            return HTTPFound(location='/', headers=headers)
 
-    if check_login(login, password):
-        headers = remember(request, login)
-        return HTTPFound(location='/', headers=headers)
-
-    message = 'Failed login'
-    return {'app_url': request.application_url,
+        message = 'Failed login'
+        return {
+            'app_url': self.request.application_url,
             'message': message,
             'menu_links': menu_links_out,
-            'footer_links': footer_links}
+            'footer_links': footer_links
+        }
 
+    @view_config(route_name='logout')
+    def logout(self):
+        headers = forget(self.request)
+        return HTTPFound(location='/', headers=headers)
 
-@view_config(route_name='logout')
-def logout(request):
-    headers = forget(request)
-    return HTTPFound(location='/', headers=headers)
+    @view_config(route_name='signup',
+                 renderer='templates/signup.pt',
+                 request_method='POST')
+    def signup(self):
+        login = self.request.params.get('login')
+        login_db = DBSession().query(Users).filter_by(login=login).first()
+        hashed_password = self.request.params.get('hash')
 
-
-@view_config(route_name='home',
-             renderer='templates/home.pt',
-             permission='view')
-def home(request):
-    userid = authenticated_userid(request)
-    num_boxes = 6
-    if 'num_boxes' in request.params:
-        if request.params['num_boxes']:
-            num_boxes = request.params['num_boxes']
-    return {'app_url': request.application_url,
-            'menu_links': menu_links_in,
-            'footer_links': footer_links,
-            'boxes': generate_box(num_boxes),
-            'user': userid}
-
-
-@view_config(route_name='signup',
-             renderer='templates/signup.pt',
-             request_method='POST')
-def signup(request):
-    session = DBSession()
-
-    login = request.params.get('login')
-
-    # print '####################################'
-    # for each in request.params:
-    #     print each
-    # print '####################################'
-
-    login_db = session.query(Users).filter_by(login=login).first()
-
-    # password = request.params.get('password1')
-    # passwordCheck = request.params.get('password2')
-    hashed_password = request.params.get('hash')
-    # print '####################################'
-    # print password
-    # print passwordCheck
-    # print hashed_password
-    # print '####################################'
-
-    if login_db:
-        return {'app_url': request.application_url,
+        if login_db:
+            return {
+                'app_url': self.request.application_url,
                 'message': 'User with login: {}, already exists.'.format(login),
                 'login': login,
                 'menu_links': menu_links_out,
-                'footer_links': footer_links}
-        # if password != confirm:
-    #     return {'app_url': request.application_url,
-    #             'message': 'Passwords doesnt match',
-    #             'login': login}
+                'footer_links': footer_links
+            }
 
-    user = Users(login, hashed_password, 1)
-    session.add(user)
-    headers = remember(request, login)
-    return HTTPFound(location='/', headers=headers)
+        user = Users(login, hashed_password, 1)
+        session.add(user)
+        headers = remember(self.request, login)
+        return HTTPFound(location='/', headers=headers)
 
-
-@view_config(route_name='signup',
-             renderer='templates/signup.pt',
-             request_method='GET')
-def signup_form(request):
-    return {'app_url': request.application_url,
+    @view_config(route_name='signup',
+                 renderer='templates/signup.pt',
+                 request_method='GET')
+    def signup_form(self):
+        return {
+            'app_url': self.request.application_url,
             'message': 'Please SignUp and join the Party!',
             'login': '',
             'menu_links': menu_links_out,
-            'footer_links': footer_links}
+            'footer_links': footer_links
+        }
+
+
+class HomeView(object):
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='home',
+                 renderer='templates/home.pt',
+                 permission='view')
+    def home(self):
+        userid = authenticated_userid(self.request)
+        num_boxes = 6
+        if 'num_boxes' in self.request.params:
+            if self.request.params['num_boxes']:
+                num_boxes = self.request.params['num_boxes']
+        return {
+            'app_url': self.request.application_url,
+            'menu_links': menu_links_in,
+            'footer_links': footer_links,
+            'boxes': generate_box(num_boxes),
+            'user': userid
+        }
 
 
 def generate_box(count):
-    """function that generates instances
-    of Div class, count must be int of how many
-    instances you need
-    return generator object
+    """receives: int(count)
+    returns: list with dictionaries
+    each dictionary will have unique keys id and link
     """
     count = int(count)
     if count <= 1:
